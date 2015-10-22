9cca174927b20d6db8cedb3d1cd3fb3d4380d8ca
Rui Zheng <zhrud21@gmail.com>
2015-10-21 17:58:38 -0700

    Solved Q8

---
+++ 8. Largest product in a series/main.py
@@ -0,0 +1,7 @@
+__author__ = 'Ray'
+
+import subprocess
+
+subprocess.check_output([r"time", r"./Largest_product_in_a_series.py"])
+subprocess.check_output([r"time", r"./Largest_product_in_a_series_Up.py"])
+# subprocess.Popen([r"time python Largest_product_in_a_series.py"])
